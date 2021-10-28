2321422478d52295cc07c68830b17e430344c10a
balt0012 <balt0012@stud.kea.dk>
2021-10-28 09:55:35 +0200

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,29 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String wordToGuess = "string";
+        String guessedLetters = "";
+        int lives = 6;
+
+        while (lives > 0){
+
+        }
+    }
+
+    public static String guessLetter(String letter, String guessedLetters, String wordToGuess, int lives){
+        if(!guessedLetters.contains(letter)){
+            guessedLetters.concat(letter);
+            if(wordToGuess.contains(letter)){
+                System.out.println("letter is in the word, good guess!");
+            } else {
+                System.out.println("letter is not in word, bad guess");
+                lives -= 1;
+            }
+        } else {
+            System.out.println("letter already guessed");
+        }
+        return guessedLetters;
+    }
 }
